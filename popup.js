f5fde48572e00be6ed9029f36566b6f8a6a37ff8
JJ <jerez.solis@gmail.com>
2023-06-17 18:02:54 -0700

    finished implementation for popup.js

--- popup.js
+++ popup.js
@@ -1,27 +1,37 @@
-import config from './config.js';
-
 document.addEventListener('DOMContentLoaded', function() {
-  var convertButton = document.getElementById('convert-button');
-  var usdInput = document.getElementById('usd-input');
-  var resultContainer = document.getElementById('result-container');
-
-  convertButton.addEventListener('click', function() {
-    var usdAmount = parseFloat(usdInput.value);
-
-    if (!isNaN(usdAmount)) {
-      fetch('https://openexchangerates.org/api/latest.json?app_id=' + config.API_KEY)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          var exchangeRate = data.rates.CAD;
-          var cadAmount = usdAmount * exchangeRate;
-
-          resultContainer.innerText = usdAmount.toFixed(2) + ' USD = ' + cadAmount.toFixed(2) + ' CAD';
-        })
-        .catch(function(error) {
-          console.log('Error:', error);
-        });
-    }
+    var convertButton = document.getElementById('convert-button');
+    var usdInput = document.getElementById('usd-input');
+    var resultContainer = document.getElementById('result-container');
+  
+    convertButton.addEventListener('click', function() {
+      var usdAmount = parseFloat(usdInput.value);
+  
+      if (!isNaN(usdAmount)) {
+        fetch('./config.json')
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            var apiKey = data.API_KEY;
+  
+            fetch('https://openexchangerates.org/api/latest.json?app_id=' + apiKey)
+              .then(function(response) {
+                return response.json();
+              })
+              .then(function(data) {
+                var exchangeRate = data.rates.CAD;
+                var cadAmount = usdAmount * exchangeRate;
+  
+                resultContainer.innerText = usdAmount.toFixed(2) + ' USD = ' + cadAmount.toFixed(2) + ' CAD';
+              })
+              .catch(function(error) {
+                console.log('Error:', error);
+              });
+          })
+          .catch(function(error) {
+            console.log('Error:', error);
+          });
+      }
+    });
   });
-});
+  
